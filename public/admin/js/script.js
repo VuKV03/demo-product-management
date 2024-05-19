11ca98ebfce05257d74d3982f89b0f3b0d837daa
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-05-19 11:29:40 +0700

    build: permanently delete

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -143,3 +143,27 @@ if (formChangeMulti) {
 }
 
 // End form-change-multi
+
+// Delete
+const buttonsDelete = document.querySelectorAll("[button-delete]");
+if (buttonsDelete) {
+  const formDeleteItem = document.querySelector("#form-delete-item");
+  const path = formDeleteItem.getAttribute("data-path");
+
+  buttonsDelete.forEach((button) => {
+    button.addEventListener("click", () => {
+      const confirmDelete = confirm("Xóa là mất đấyy!!");
+      if(confirmDelete) {
+        const id = button.getAttribute("data-id");
+
+        const action = path + `/${id}?_method=DELETE`;
+
+        formDeleteItem.action = action;
+
+        formDeleteItem.submit();
+      }
+      
+    });
+  });
+}
+// End Delete
