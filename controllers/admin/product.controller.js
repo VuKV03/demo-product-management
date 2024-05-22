9788b79ba2eba5ecffe7e8449f36606718df44ee
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-05-22 07:58:28 +0700

    build: change position

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -39,6 +39,7 @@ module.exports.index = async (req, res) => {
   // End Pagination
 
   const products = await Product.find(find)
+    .sort({ position: "desc"})
     .limit(objectPagination.limitItems)
     .skip(objectPagination.skip);
 
@@ -93,6 +94,13 @@ module.exports.changeMulti = async (req, res) => {
           deletedAt: new Date(),
         }
       );
+      break;
+
+    case "change-position":
+      for(const item of ids) {
+        const [id, position] = item.split("-");
+        await Product.updateOne({ _id: id }, { position: position});
+      }
       break;
 
     default:
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -140,8 +140,18 @@ if (formChangeMulti) {
       const inputIds = formChangeMulti.querySelector("input[name='ids']");
 
       inputsChecked.forEach((input) => {
-        ids.push(input.value);
+        const id = input.value;
+        
+        if(typeChange == "change-position") {
+          const position = input.closest("tr").querySelector("input[name='position']").value;
+
+          ids.push(`${id}-${position}`)
+        } else {
+          ids.push(id);
+        }
       });
+
+      
 
       inputIds.value = ids.join(", ");
 
