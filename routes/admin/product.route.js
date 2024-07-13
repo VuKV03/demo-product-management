df664b38eae0112e3a321543d65ff292c18c96f5
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-07-13 16:11:00 +0700

    build: validate title

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -9,6 +9,8 @@ const upload = multer({ storage: storage });
 
 const controller = require("../../controllers/admin/product.controller");
 
+const validate = require("../../validates/admin/product.validate");
+
 router.get("/", controller.index);
 
 router.patch("/change-status/:status/:id", controller.changeStatus);
@@ -19,6 +21,10 @@ router.delete("/delete/:id", controller.deleteItem);
 
 router.get("/create", controller.create);
 
-router.post("/create", upload.single("thumbnail"), controller.createPost);
+router.post(
+  "/create", 
+  upload.single("thumbnail"), 
+  validate.createPost,
+  controller.createPost);
 
 module.exports = router;
