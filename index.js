939bff1aa2c42a1b3500912091ffb2c1fcdfd7d2
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-07-15 23:54:27 +0700

    build: deploy vercel

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ const routeClient = require("./routes/client/index.route");
 const app = express();
 const port = process.env.PORT;
 
-app.set("views", "./views");
+app.set('views', `${__dirname}/views`);
 app.set("view engine", "pug");
 
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 
 // Flash
 app.use(cookieParser('Ghi gì cũng được'));
