209257c11fb52cd305b1377d834556dd8257bde8
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-07-15 23:07:51 +0700

    build: page detail product (client)

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -4,7 +4,7 @@ const Product = require("../../models/product.model");
 module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: "active",
-    deleted: false
+    deleted: false,
   }).sort({ position: "desc" });
 
   const newProducts = products.map((item) => {
@@ -20,4 +20,24 @@ module.exports.index = async (req, res) => {
     pageTitle: "Trang danh sách sản phẩm",
     products: newProducts,
   });
+};
+
+// [GET] /products/detail/:slug
+module.exports.detail = async (req, res) => {
+  try {
+    const slug = req.params.slug;
+    const product = await Product.findOne({
+      slug: slug,
+      deleted: false,
+      status: "active"
+    });
+    console.log(product)
+
+    res.render("client/pages/products/detail", {
+      pageTitle: "Trang chi tiết sản phẩm",
+      product: product
+    });
+  } catch (error) {
+    res.redirect("/");
+  }
 };
