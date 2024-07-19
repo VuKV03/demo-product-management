3b289b49ed56ec2718d49741ff28d4f40578e7e9
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-07-19 14:47:33 +0700

    build: edit account

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -1,4 +1,4 @@
-const md5 = require('md5');
+const md5 = require("md5");
 const Account = require("../../models/account.model");
 const Role = require("../../models/role.model");
 
@@ -10,12 +10,12 @@ module.exports.index = async (req, res) => {
     deleted: false,
   });
 
-  for(const record of records) {
+  for (const record of records) {
     const role = await Role.findOne({
       _id: record.role_id,
       deleted: false,
-    })
-    
+    });
+
     record.roleTitle = role.title;
   }
 
@@ -33,15 +33,57 @@ module.exports.create = async (req, res) => {
 
   res.render("admin/pages/accounts/create", {
     pageTitle: "Tạo mới tài khoản",
-    roles: roles
+    roles: roles,
   });
 };
 
 // [POST] /admin/accounts/create
 module.exports.createPost = async (req, res) => {
-  req.body.password = md5(req.body.password)
+  req.body.password = md5(req.body.password);
   const record = new Account(req.body);
   await record.save();
 
   res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
+};
+
+// [GET] /admin/accounts/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const find = {
+      _id: req.params.id,
+      deleted: false,
+    };
+
+    const data = await Account.findOne(find);
+
+    const roles = await Role.find({
+      deleted: false,
+    });
+
+    res.render("admin/pages/accounts/edit", {
+      pageTitle: "Chỉnh sửa tài khoản",
+      data: data,
+      roles: roles,
+    });
+  } catch (error) {
+    res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
+  }
+};
+
+// [PATCH] /admin/accounts/edit/:id
+module.exports.editPatch = async (req, res) => {
+  if (req.body.password) {
+    req.body.password = md5(req.body.password);
+  } else {
+    delete req.body.password;
+  }
+
+  await Account.updateOne(
+    {
+      _id: req.params.id,
+    },
+    req.body
+  );
+
+  res.redirect("back");
 };
