fdd04b311aa9f112b419e455d08ae47e5bf95d47
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-07-23 15:26:15 +0700

    build: list of new products

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -13,8 +13,21 @@ module.exports.index = async (req, res) => {
   const newProductsFeatured = productsHelper.newProductsPrice(productsFeatured);
   // Hiển thị danh sách sản phẩm nổi bật
 
+  // Hiển thị danh sách sản phẩm mới nhất
+  const listProducts = await Product.find({
+    deleted: false,
+    status: "active",
+  })
+    .sort({ position: "desc" })
+    .limit(6);
+
+  const newProducts = productsHelper.newProductsPrice(listProducts);
+
+  // Hiển thị danh sách sản phẩm mới nhất
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
     productsFeatured: newProductsFeatured,
+    newProducts: newProducts,
   });
 };
