546b49838ab253b043db8cf39123d632174cc8c2
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-07-23 15:39:31 +0700

    build: (update) List of new products

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,9 +1,34 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
 const productsHelper = require("../../helpers/products");
 
 // [GET] /products
 module.exports.index = async (req, res) => {
   const products = await Product.find({
+    status: "active",
+    deleted: false,
+  }).sort({ position: "desc" });
+
+  const newProducts = productsHelper.newProductsPrice(products);
+
+  res.render("client/pages/products/index", {
+    pageTitle: "Trang danh sách sản phẩm",
+    products: newProducts,
+  });
+};
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  const slugCategory = req.params.slugCategory;
+
+  const category = await ProductCategory.findOne({
+    slug: slugCategory,
+    deleted: false,
+    status: "active",
+  });
+
+  const products = await Product.find({
+    product_category_id: category.id,
     status: "active",
     deleted: false,
   }).sort({ position: "desc" });
