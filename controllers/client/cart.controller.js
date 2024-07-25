0b58cd12a26d6cb07118848ea44b1dda2cfa01a9
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-07-25 10:33:10 +0700

    build: cart page

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,34 @@
 const Cart = require("../../models/cart.model");
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
+
+// [GET] /cart/
+module.exports.index = async (req, res) => {
+  const cart = await Cart.findOne({
+    _id: req.cookies.cartId,
+  });
+
+  cart.totalPrice = 0;
+
+  for (const item of cart.products) {
+    const infoProduct = await Product.findOne({
+      _id: item.product_id,
+    }).select("thumbnail title price discountPercentage stock slug");
+
+    infoProduct.newPrice = productsHelper.newProductPrice(infoProduct);
+
+    infoProduct.totalPrice = infoProduct.newPrice * item.quantity;
+
+    cart.totalPrice += infoProduct.totalPrice;
+
+    item.infoProduct = infoProduct;
+  }
+
+  res.render("client/pages/cart/index", {
+    pageTitle: "Giỏ hàng",
+    cartDetail: cart,
+  });
+};
 
 // [POST] /cart/add/:productId
 module.exports.addPost = async (req, res) => {
