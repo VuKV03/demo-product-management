d49471d2dc63d284a559367480009f581e24fe81
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-07-25 14:06:06 +0700

    build: save info db

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -1,20 +1,24 @@
 const Cart = require("../../models/cart.model");
 const Product = require("../../models/product.model");
+const Order = require("../../models/order.model");
 
 // [GET] /checkout/
 module.exports.index = async (req, res) => {
   const cart = await Cart.findOne({
-    _id: req.cookies.cartId
+    _id: req.cookies.cartId,
   });
 
   cart.totalPrice = 0;
 
   for (const item of cart.products) {
     const infoProduct = await Product.findOne({
-      _id: item.product_id
+      _id: item.product_id,
     }).select("thumbnail title price discountPercentage stock slug");
 
-    infoProduct.newPrice = (infoProduct.price * (100 - infoProduct.discountPercentage)/100).toFixed(0);
+    infoProduct.newPrice = (
+      (infoProduct.price * (100 - infoProduct.discountPercentage)) /
+      100
+    ).toFixed(0);
 
     infoProduct.totalPrice = infoProduct.newPrice * item.quantity;
 
@@ -25,6 +29,54 @@ module.exports.index = async (req, res) => {
 
   res.render("client/pages/checkout/index", {
     pageTitle: "Đặt hàng",
-    cartDetail: cart
+    cartDetail: cart,
   });
+};
+
+// [POST] /checkout/order
+module.exports.order = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const userInfo = req.body;
+
+  const cart = await Cart.findOne({
+    _id: cartId,
+  });
+
+  const products = [];
+
+  for (const item of cart.products) {
+    const product = await Product.findOne({
+      _id: item.product_id,
+    });
+
+    const objectProduct = {
+      product_id: item.product_id,
+      price: product.price,
+      discountPercentage: product.discountPercentage,
+      quantity: item.quantity,
+    };
+
+    products.push(objectProduct);
+  }
+
+  const dataOrder = {
+    // user_id: String,
+    cart_id: cartId,
+    userInfo: userInfo,
+    products: products,
+  };
+
+  const order = new Order(dataOrder);
+  await order.save();
+
+  await Cart.updateOne(
+    {
+      _id: cartId,
+    },
+    {
+      products: [],
+    }
+  );
+
+  res.redirect(`/checkout/success/${order.id}`);
 };
