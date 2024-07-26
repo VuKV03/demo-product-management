b87aeb8bb1d4ae2a9bcb01c44b147088e98d5ed4
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-07-26 22:17:28 +0700

    build: (update) forgot password

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -144,17 +144,43 @@ module.exports.otpPasswordPost = async (req, res) => {
     otp: otp,
   });
 
-  if(!result) {
+  if (!result) {
     req.flash("error", `OTP không hợp lệ!`);
     res.redirect("back");
     return;
   }
 
   const user = await User.findOne({
-    email: email
+    email: email,
   });
 
   res.cookie("tokenUser", user.tokenUser);
 
   res.redirect("/user/password/reset");
+};
+
+// [GET] /user/password/reset
+module.exports.resetPassword = async (req, res) => {
+  res.render("client/pages/user/reset-password", {
+    pageTitle: "Đổi mật khẩu",
+  });
+};
+
+// [GET] /user/password/reset
+module.exports.resetPasswordPost = async (req, res) => {
+  const password = req.body.password;
+  const tokenUser = req.cookies.tokenUser;
+
+  await User.updateOne(
+    {
+      tokenUser: tokenUser,
+    },
+    {
+      password: md5(password),
+    }
+  );
+
+  req.flash("success", "Đổi mật khẩu thành công!");
+
+  res.redirect("/");
 };
--- routes/client/user.route.js
+++ routes/client/user.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../../controllers/client/user.controller");
-const validate = require("../../validates/client/user.validate")
+const validate = require("../../validates/client/user.validate");
 
 router.get("/register", controller.register);
 
@@ -10,16 +10,28 @@ router.post("/register", validate.registerPost, controller.registerPost);
 
 router.get("/login", controller.login);
 
-router.post("/login",validate.loginPost, controller.loginPost);
+router.post("/login", validate.loginPost, controller.loginPost);
 
 router.get("/logout", controller.logout);
 
 router.get("/password/forgot", controller.forgotPassword);
 
-router.post("/password/forgot", validate.forgotPasswordPost, controller.forgotPasswordPost);
+router.post(
+  "/password/forgot",
+  validate.forgotPasswordPost,
+  controller.forgotPasswordPost
+);
 
 router.get("/password/otp", controller.otpPassword);
 
 router.post("/password/otp", controller.otpPasswordPost);
+
+router.get("/password/reset", controller.resetPassword);
+
+router.post(
+  "/password/reset",
+  validate.resetPasswordPost,
+  controller.resetPasswordPost
+);
 
 module.exports = router;
