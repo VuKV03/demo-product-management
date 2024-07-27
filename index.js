6c7e441d836292263164fb4af5225c95a12ce368
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-07-27 15:18:01 +0700

    build: 404

--- index.js
+++ index.js
@@ -60,6 +60,14 @@ routeAdmin(app);
 routeClient(app);
 // End Routes
 
+// 404
+app.get("*", (req, res) => {
+  res.render("client/pages/errors/404", {
+    pageTitle: "404 Not Found",
+  });
+});
+// 404
+
 app.listen(port, () => {
   console.log(`App listen on port ${port}`);
 });
