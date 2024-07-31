2c71bada206587a919358de9fac0c3300a973b66
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-07-31 14:57:37 +0700

    build: tag online

--- models/user.model.js
+++ models/user.model.js
@@ -20,6 +20,7 @@ const userSchema = new mongoose.Schema(
     ],
     acceptFriends: Array, // Lời mời dã nhận
     requestFriends: Array, // Lời mời đã gửi
+    statusOnline: String,
     status: {
       type: String,
       default: "active"
