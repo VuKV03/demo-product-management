b73da164330950f89b2c92767ec4707e49f23cb0
Khuất Văn Vũ <khuatvanvu03@gmail.com>
2024-08-01 22:45:41 +0700

    build: (update) show alert

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -186,18 +186,18 @@ if (buttonDelete.length) {
 // End Delete
 
 // Show alert
-const showAlert = document.querySelector("[show-alert]");
-if (showAlert) {
-  const time = parseInt(showAlert.getAttribute("data-time")) || 3000;
-  const closeAlert = showAlert.querySelector("[close-alert]");
-  setTimeout(() => {
-    showAlert.classList.add("alert-hidden");
-  }, time);
+// const showAlert = document.querySelector("[show-alert]");
+// if (showAlert) {
+//   const time = parseInt(showAlert.getAttribute("data-time")) || 3000;
+//   const closeAlert = showAlert.querySelector("[close-alert]");
+//   setTimeout(() => {
+//     showAlert.classList.add("alert-hidden");
+//   }, time);
 
-  closeAlert.addEventListener("click", () => {
-    showAlert.classList.add("alert-hidden");
-  });
-}
+//   closeAlert.addEventListener("click", () => {
+//     showAlert.classList.add("alert-hidden");
+//   });
+// }
 
 // End Show alert
 
